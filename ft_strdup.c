976791179d15b36cc42d8dcc909ea1e5e1199ac5
Bradleigh MARKS <bmarks@c3r1s8.wethinkcode.co.za>
2019-05-22 14:40:39 +0200

    22 May 19 - 14:41

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: bmarks <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 11:09:26 by bmarks            #+#    #+#             */
-/*   Updated: 2019/05/22 11:37:56 by bmarks           ###   ########.fr       */
+/*   Updated: 2019/05/22 14:40:09 by bmarks           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,7 @@ char	*ft_strdup(const char *s)
 	i = 0;
 	ns = (char *)malloc(ft_strlen(s) + 1);
 	if (!ns)
-	{
-		printf("ah sjit fam malloc failed");
 		return (NULL);
-	}
 	while (*(s + i))
 	{
 		*(ns + i) = *(s + i);
