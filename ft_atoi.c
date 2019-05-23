f4f2fc5c29efe2cb3cd31602359c3864a81d5534
Bradleigh MARKS <bmarks@c3r1s8.wethinkcode.co.za>
2019-05-23 12:03:26 +0200

    23 May 19 12:04

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: bmarks <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 12:42:44 by bmarks            #+#    #+#             */
-/*   Updated: 2019/05/22 13:11:44 by bmarks           ###   ########.fr       */
+/*   Updated: 2019/05/23 12:01:35 by bmarks           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,27 @@ int	ft_atoi(const char *s)
 {
 	int	i;
 	int	n;
+	int	sign;
 
 	i = 0;
 	n = 0;
+	sign = 1;
+	while (ft_isspace(*(s + i)))
+		i++;
+	if (*(s + i) == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (*(s + i) == '+')
+	{
+		i++;
+	}
 	while (*(s + i) && ft_isdigit(*(s + i)))
 	{
 		n = n * 10 + *(s + i) - '0';
 		i++;
 	}
+	n = n * sign;
 	return (n);
 }
