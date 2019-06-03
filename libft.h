34d63336b9380bc95d5432530ee9bb223d9e25fd
Bradleigh MARKS <bmarks@c3r1s7.wethinkcode.co.za>
2019-06-03 09:18:06 +0200

    libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bmarks <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/15 12:31:33 by bmarks            #+#    #+#             */
-/*   Updated: 2019/05/28 15:13:08 by bmarks           ###   ########.fr       */
+/*   Updated: 2019/05/30 12:54:48 by iisaacs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,5 +96,6 @@ size_t				ft_strspn(const char *s1, const char *s2);
 char				*ft_strpbrk(const char *s1, const char *s2);
 char				*ft_strtok(char *s1, const char *s2);
 char				*ft_strtok_r(char *s1, const char *s2, char **lasts);
+void				ft_rotx(char *s, int x);
 
 #endif
