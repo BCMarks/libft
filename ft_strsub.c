43d74155a4b8d434e155722e93a31670e6b739d7
Bradleigh MARKS <bmarks@c3r5s1.wethinkcode.co.za>
2019-06-04 16:29:26 +0200

    ft_strsub

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: bmarks <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 14:06:18 by bmarks            #+#    #+#             */
-/*   Updated: 2019/05/24 14:45:41 by bmarks           ###   ########.fr       */
+/*   Updated: 2019/06/04 16:27:58 by bmarks           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 
 	sub = (char *)malloc(sizeof(char) * ((unsigned int)len + 1));
 	ft_strncpy(sub, (s + start), len);
-	*(sub + (unsigned int)len + 1) = '\0';
+	*(sub + (unsigned int)len) = '\0';
 	if (!sub)
 		return (NULL);
 	return (sub);
